1b8fbd9f7370c5692f74b3f8a39818a7098a8c6e
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-07-30 08:30:28 +0530

    5000 server running and create an APi to get all booking data

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlaxhk5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// async function error
+async function run() {
+  try {
+    await client.connect();
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
+    const appointmentCollection = client
+      .db("doctors_portal")
+      .collection("appointment");
+
+    //  get multiple data
+    app.get("/bookings", async (req, res) => {
+      const query = {};
+      const cursor = bookingCollection.find(query);
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Doctors Portal!");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors app listening on port ${port}`);
+});
